98ebb083ad034a5b5deb25c5fff4ce1f213f920c
Lucille81 <milakis2601@gmail.com>
2022-07-11 23:03:00 +0300

    task done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,44 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const OPEN_BRACKETS = ['(', '{', '[', '1', '3', '5'];
+  const BRACKETS_PAIR = {
+    [')']: '(',
+    ['}']: '{',
+    [']']: '[',
+    ['2']: '1',
+    ['4']: '3',
+    ['6']: '5',
+  };
+
+  let stack = [];
+  for (let i = 0; i < str.length; i++) {
+    let currentSymbol = str[i];
+
+    if (OPEN_BRACKETS.includes(currentSymbol)) {
+      stack.push(currentSymbol);
+    } else if ('|78'.includes(currentSymbol)) {
+      if (stack.length === 0) {
+        stack.push(currentSymbol);
+      } else {
+        if (currentSymbol === stack[stack.length - 1]) {
+          stack.pop();
+        } else {
+          stack.push(currentSymbol);
+        }
+      }
+    } else {
+      if (stack.length === 0) {
+        return false;
+      }
+
+      let topElement = stack[stack.length - 1];
+
+      if (BRACKETS_PAIR[currentSymbol] === topElement) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0;
+};
